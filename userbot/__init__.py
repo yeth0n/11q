8963d13c793681252ce4afcab4ec50e721773493
botati <84269812+botati@users.noreply.github.com>
2022-02-27 05:06:58 +0300

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -6,6 +6,8 @@
 # Please read the GNU Affero General Public License in;
 # < https://github.com/JMTHON-AR/JM-THON/blob/master/LICENSE
 # ===============================================================
+import signal
+import sys
 import time
 
 import heroku3
@@ -13,25 +15,32 @@ import heroku3
 from .Config import Config
 from .core.logger import logging
 from .core.session import jmthon
+from .helpers.utils.utils import runasync
 from .sql_helper.globals import addgvar, delgvar, gvarstatus
 
-__version__ = "1.0.0"
-__license__ = "كـتابة وتـعديل فريـق جـمثون"
-__author__ = "جـمثون <https://T.ME/JMTHON>"
-__copyright__ = "JMTHON TEAM (C) 2020 - 2021  " + __author__
+__version__ = "3.0.6"
+__license__ = "رخصة جنو أفيرو العمومية v3.0"
+__author__ = "jmthon <https://github.com/JMTHON-AR/jmthon>"
+__copyright__ = f"حقوق جمثون (C) 2020 - 2021  {__author__}"
 
 jmthon.version = __version__
 jmthon.tgbot.version = __version__
-LOGS = logging.getLogger("جـمثون")
+LOGS = logging.getLogger("jmthon")
 bot = jmthon
 
 StartTime = time.time()
-JMVERSION = "5.2.0"
+jmthonversion = "3.0.6"
 
 
-if Config.UPSTREAM_REPO == "Jmthon-iq":
-    UPSTREAM_REPO_URL = "https://github.com/JMTHON-AR/JM-THON"
-elif Config.UPSTREAM_REPO == "jmthon-ar":
+def close_connection(*_):
+    print("تم اغلاق اتصال السورس.")
+    runasync(jmthon.disconnect())
+    sys.exit(143)
+
+
+signal.signal(signal.SIGTERM, close_connection)
+
+if Config.UPSTREAM_REPO == "jmthon-ar":
     UPSTREAM_REPO_URL = "https://github.com/JMTHON-AR/JM-THON"
 else:
     UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
@@ -58,6 +67,7 @@ if Config.PM_LOGGER_GROUP_ID == 0:
         Config.PM_LOGGER_GROUP_ID = int(gvarstatus("PM_LOGGER_GROUP_ID"))
 elif str(Config.PM_LOGGER_GROUP_ID)[0] != "-":
     Config.PM_LOGGER_GROUP_ID = int("-" + str(Config.PM_LOGGER_GROUP_ID))
+
 try:
     if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:
         HEROKU_APP = heroku3.from_key(Config.HEROKU_API_KEY).apps()[
