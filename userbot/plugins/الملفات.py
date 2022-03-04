f4952d77b703b3413e37e6cf8d90d30fc990e165
botati <84269812+botati@users.noreply.github.com>
2022-03-04 23:53:09 +0300

    Update and rename externalplugins.py to الملفات.py

--- userbot/plugins/الملفات.py
+++ userbot/plugins/الملفات.py
@@ -6,14 +6,14 @@ from telethon.tl.types import InputMessagesFilterDocument
 from ..Config import Config
 from ..helpers.utils import install_pip
 from ..utils import load_module
-from . import BOTLOG, BOTLOG_CHATID, catub
+from . import BOTLOG, BOTLOG_CHATID, jmthon
 
 plugin_category = "tools"
 
 if Config.PLUGIN_CHANNEL:
 
     async def install():
-        documentss = await catub.get_messages(
+        documentss = await jmthon.get_messages(
             Config.PLUGIN_CHANNEL, None, filter=InputMessagesFilterDocument
         )
         total = int(documentss.total)
@@ -22,8 +22,8 @@ if Config.PLUGIN_CHANNEL:
             plugin_name = documentss[module].file.name
             if os.path.exists(f"userbot/plugins/{plugin_name}"):
                 return
-            downloaded_file_name = await catub.download_media(
-                await catub.get_messages(Config.PLUGIN_CHANNEL, ids=plugin_to_install),
+            downloaded_file_name = await jmthon.download_media(
+                await jmthon.get_messages(Config.PLUGIN_CHANNEL, ids=plugin_to_install),
                 "userbot/plugins/",
             )
             path1 = Path(downloaded_file_name)
@@ -40,9 +40,9 @@ if Config.PLUGIN_CHANNEL:
                     if check > 5:
                         break
             if BOTLOG:
-                await catub.send_message(
+                await jmthon.send_message(
                     BOTLOG_CHATID,
                     f"الملف `{os.path.basename(downloaded_file_name)}` تم تثبيته بنجاح.",
                 )
 
-    catub.loop.create_task(install())
+    jmthon.loop.create_task(install())
